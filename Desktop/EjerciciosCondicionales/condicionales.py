7b2740b9fdb565f3b257dde1929435a1f5ec3d04
Dgonzalez44 <dgonzale44@cuc.edu.co>
2021-09-13 14:06:04 -0500

    Ejercicio 2

--- Desktop/EjerciciosCondicionales/condicionales.py
+++ Desktop/EjerciciosCondicionales/condicionales.py
@@ -6,3 +6,13 @@ Created on Mon Sep 13 12:22:26 2021
 """
 
 # Ejercicio #1
+cantidad = int(input("Ingrese la cantidad de camisas compradas: " ))
+v = int(input("Ingrese el valor de las camisas que compro: $"))
+
+if cantidad >= 3:
+	t = v*0.30
+else:
+    t = v*0.10
+print(f"el total a pagar: ${v-t}")
+print(f"el descuento aplicado es: ${t}")
+
